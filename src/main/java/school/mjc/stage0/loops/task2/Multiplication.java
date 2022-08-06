feda9efcbd50f617b9c85d13edeb5ecb0d116d14
gabrlaur <45432522+gabrlaur@users.noreply.github.com>
2022-08-06 17:56:36 +0300

    Task #2 fix

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -3,9 +3,16 @@ package school.mjc.stage0.loops.task2;
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
         int counter = 0;
-        while (counter <= multiplyByAndToInclusive) {
-            System.out.println(counter * multiplyByAndToInclusive);
-            counter++;
+        if (multiplyByAndToInclusive > 0) {
+            while (counter <= multiplyByAndToInclusive) {
+                System.out.println(counter * multiplyByAndToInclusive);
+                counter++;
+            }
+        } else if (multiplyByAndToInclusive < 0) {
+            while (counter <= Math.abs(multiplyByAndToInclusive)) {
+                System.out.println(counter * multiplyByAndToInclusive);
+                counter++;
+            }
         }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/NaturalNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/NaturalNumbers.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.loops.task2;
 
 public class NaturalNumbers {
     public void naturalNumbersPrinter(int lastPrinted) {
-        int counter = 1;
+        int counter = 0;
         while (counter <= lastPrinted) {
             System.out.println(counter);
             counter++;
